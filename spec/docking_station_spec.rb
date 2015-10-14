75436cd720c93de45cecf5100b3ad13110c03658
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-10-14 10:52:34 +0100

    bike responds to working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,11 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
+  docking_station = DockingStation.new
+  bike = docking_station.release_bike
+  describe bike do
+    it { is_expected.to respond_to :working?}
+  end
+
+
 end
