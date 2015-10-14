4765a872c8c2689ef1edf91be687a33fed168a1e
Chuka Ebi <chuka.ebi@outlook.com>
2015-10-14 11:27:57 +0100

    Added docks_bike and check to the DockingStation class

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,19 @@
 require_relative 'bike'
-
 class DockingStation
+  attr_reader :bike
+  def initialize
+    @bike = []
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def docks_bike(bike)
+    @bike << bike
+  end
+
+  def check
+    @bike.last
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,9 +5,16 @@ describe DockingStation do
 
   docking_station = DockingStation.new
   bike = docking_station.release_bike
+
   describe bike do
     it { is_expected.to respond_to :working?}
   end
 
+  it { is_expected.to respond_to(:docks_bike).with(1).argument }
+
+  docking_station.docks_bike(bike)
+  describe docking_station.bike do
+    it { is_expected.to include(bike)}
+  end
 
 end
