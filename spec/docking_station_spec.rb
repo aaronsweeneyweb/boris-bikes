cb8fa1f8c42aa95fa6964978bf47f0ce7051feb7
thisdotrob <thisdotrob@gmail.com>
2015-10-15 20:19:44 +0100

    Passed test for docking bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,12 @@ describe DockingStation do
     expect(subject.release_bike).to be_working
   end
 
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+  it 'can see a docked bike' do
+    bike1 = Bike.new
+    subject.dock_bike(bike1)
+    expect(subject.bike).to eq bike1
+  end
+
 end
