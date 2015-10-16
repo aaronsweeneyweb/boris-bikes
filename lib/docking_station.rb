ed451cc7e99a95f6bff13de0d9aae6f8fea7caf3
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-10-16 10:43:46 +0100

    passed test to raise an error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,6 +5,7 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
+    fail 'No bikes available' unless @bike
     Bike.new
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,7 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases a working bike' do
+    subject.dock_bike(Bike.new)
     expect(subject.release_bike).to be_working
   end
 
@@ -13,6 +14,10 @@ describe DockingStation do
     bike1 = Bike.new
     subject.dock_bike(bike1)
     expect(subject.bike).to eq bike1
+  end
+
+  it 'should raise an error when no bikes available' do
+  expect {subject.release_bike}.to raise_error
   end
 
 end
