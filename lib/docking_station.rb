adbb530d0dcd51b96aaf86b6619ade9e100f7d22
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-10-16 12:24:50 +0100

    used single responsibility principle to refactor code

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,15 +7,24 @@ class DockingStation
   def initialize
     @bikes = []
   end
- 
+
   def release_bike
-    fail 'No bikes available' unless @bikes.length > 0
+    fail 'No bikes available' if empty?
     Bike.new
   end
 
   def dock_bike(bike)
-    fail 'Docking Station is full' if @bikes.length >= 20
+    fail 'Docking Station is full' if full?
     @bikes <<  bike
+  end
+
+  private
+  def full?
+    @bikes.length >= 20 #evaluates to true/false
+  end
+
+  def empty?
+    @bikes.length == 0
   end
 
 end
